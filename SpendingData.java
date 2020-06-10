5330c3df083b537e9cc5e122409e9aeeaceb1d2c
MCMitchell8 <65644728+MCMitchell8@users.noreply.github.com>
2020-06-10 09:56:01 -0700

    Add files via upload
    Add spending data

---
+++ SpendingData.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BudgetUI;
+
+/**
+ *
+ * @author melissacmitchell
+ */
+
+public class SpendingData {
+    
+    //variables to hold the user's spending data
+    double totalAmountSpent = 0;
+    int numberLessThan25 = 0;
+    int number25Through50 = 0;
+    int number50Through250 = 0;
+    int number250Through500 = 0;
+    int number500Through1000 = 0;
+    int number1000Through2500 = 0;
+    int numberGreaterThan2500 = 0;
+    double costBills = 0;
+    double costKids = 0;
+    double costDining = 0;
+    double costGroceries = 0;
+    double costEntertainment = 0;
+    double costPersonal = 0;
+    double costHealth = 0;
+    double costGifts = 0;
+    double costInvestments = 0;
+    double costMiscellaneous = 0;
+}
